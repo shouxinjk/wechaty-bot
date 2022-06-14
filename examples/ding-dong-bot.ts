67aa52ad17594b9faa858af67782c9a4d7d6e980
Alex Chew <alex_chew@msn.com>
2022-06-14 18:56:47 +0800

    bug fix

--- examples/ding-dong-bot.ts
+++ examples/ding-dong-bot.ts
@@ -18,16 +18,16 @@ import {
 
 import qrcodeTerminal from 'qrcode-terminal'
 
-import { onScan } from  "../service/bot-service/scan-service.js"               // 当机器人需要扫码登陆的时候会触发这个事件。
-import { onLogin } from "../service/bot-service/login-service.js"             // 当机器人成功登陆后，会触发事件，并会在事件中传递当前登陆机器人的信息
-import { onLogout } from "../service/bot-service/logout-service.js"           // 当机器人检测到登出的时候，会触发事件，并会在事件中传递机器人的信息。
-import { onReady } from "../service/bot-service/ready-service.js"             // 当所有数据加载完成后，会触发这个事件。在wechaty-puppet-padchat 中，它意味着已经加载完成Contact 和Room 的信息。
-import { onMessage } from "../service/bot-service/message-service.js"         // 当机器人收到消息的时候会触发这个事件。
-import { onRoomInvite } from "../service/bot-service/room-invite-service.js"  // 当收到群邀请的时候，会触发这个事件。
-import { onRoomTopic } from "../service/bot-service/room-topic-service.js"    // 当有人修改群名称的时候会触发这个事件。
-import { onRoomJoin } from "../service/bot-service/room-join-service.js"      // 当有人进入微信群的时候会触发这个事件。机器人主动进入某个微信群，那个样会触发这个事件。
-import { onRoomleave } from "../service/bot-service/room-leave-service.js"    // 当机器人把群里某个用户移出群聊的时候会触发这个时间。用户主动退群是无法检测到的。
-import { onFriendship } from "../service/bot-service/friendship-service.js"   // 当有人给机器人发好友请求的时候会触发这个事件。
+import { onScan } from  '../service/bot-service/scan-service.js'               // 当机器人需要扫码登陆的时候会触发这个事件。
+import { onLogin } from '../service/bot-service/login-service.js'             // 当机器人成功登陆后，会触发事件，并会在事件中传递当前登陆机器人的信息
+import { onLogout } from '../service/bot-service/logout-service.js'           // 当机器人检测到登出的时候，会触发事件，并会在事件中传递机器人的信息。
+import { onReady } from '../service/bot-service/ready-service.js'             // 当所有数据加载完成后，会触发这个事件。在wechaty-puppet-padchat 中，它意味着已经加载完成Contact 和Room 的信息。
+import { onMessage } from '../service/bot-service/message-service.js'         // 当机器人收到消息的时候会触发这个事件。
+import { onRoomInvite } from '../service/bot-service/room-invite-service.js'  // 当收到群邀请的时候，会触发这个事件。
+import { onRoomTopic } from '../service/bot-service/room-topic-service.js'    // 当有人修改群名称的时候会触发这个事件。
+import { onRoomJoin } from '../service/bot-service/room-join-service.js'      // 当有人进入微信群的时候会触发这个事件。机器人主动进入某个微信群，那个样会触发这个事件。
+import { onRoomleave } from '../service/bot-service/room-leave-service.js'    // 当机器人把群里某个用户移出群聊的时候会触发这个时间。用户主动退群是无法检测到的。
+import { onFriendship } from '../service/bot-service/friendship-service.js'   // 当有人给机器人发好友请求的时候会触发这个事件。
 import { onHeartbeat } from '../service/bot-service/heartbeat-service.js'     // 获取机器人的心跳。
 import { onError } from '../service/bot-service/error-service.js'             // 当机器人内部出错的时候会触发error 事件。
 
@@ -112,7 +112,4 @@ bot.on('error', onError)
 bot.start()
   .then(() => log.info('StarterBot', 'Starter Bot Started.'))
   .catch(e => log.error('StarterBot', e))
-
-
-
 
